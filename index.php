9840f837b84920e30c35c06b1d79ab5f10f92a8a
claudiu-ancau-rm <claudiu.ancau@imobiliare.ro>
2017-06-07 20:59:53 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -10,5 +10,31 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     $response->getBody()->write("Hello, $name");
 
     return $response;
+$app->put('/plot/{id}', function (Request $request, Response $response)
+    global $db ;
+    $id = $request->getAttribute('id');
+    $rsp = createPlotRepresentation ($db, $id) ;
+    return $response->withJSON (array("response"=>"should return a neutral object, maybe based on plot ID..."));
+});
+
+$app->get('/plot/{id}', function (Request $request, Response $response);
+$app->put('/graph', function (Request $request, Response $response) {
+    global $db ;
+    $id = createPlotEntry ($db) ;
+    return $response->withJSON(array("response"=>array("id"=>$id))) ;
+});
+
+$app->get('/graph/{id}', function (Request $request, Response $response) {
+    global $db ;
+    $id = $request->getAttribute('id');
+    $rsp = getPlotData ($db, $id) ;
+    return $response->withJSON (array("response"=>$rsp));
+});
+
+$app->post('/graph/{id}/function/{fntype}', function (Request $request, Response $response);
+$app->post('/graph/{id}/terminal/{termtype}', function (Request $request, Response $response);
+$app->get('/graph/{id}/function', function (Request $request, Response $response);
+
+
 });
 $app->run();
